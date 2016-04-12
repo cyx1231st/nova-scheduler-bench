7d4ad701b947a70b1c4ac0a38ed3370be4de538e
cyx1231st <cyx1231st@gmail.com>
2016-04-12 02:10:09 -0500

    reset database for another run

--- 5-reset.sh
+++ 5-reset.sh
@@ -4,6 +4,12 @@ pushd $TOP_DIR > /dev/null
 
 ./openstack-bench/killall.sh >/dev/null 2>&1 &
 rm .requests >/dev/null 2>&1 &
+
+mysql --database='nova' --execute=\
+"delete from services where services.binary='nova-compute';
+delete from compute_nodes;
+delete from services where services.binary='nova-scheduler';"
+
 watch "ps aux | grep run_bench"
 
 popd > /dev/null
