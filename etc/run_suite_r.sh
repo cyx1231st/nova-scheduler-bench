709f585f0bab266269daeca48e4f94c8d2154337
Yingxin <yingxin.cheng@intel.com>
2016-04-12 15:51:17 +0800

    fix remote compute service launch

--- etc/run_suite_r.sh
+++ etc/run_suite_r.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-set -x
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 pushd $TOP_DIR > /dev/null
 
+if [ $# -eq 2 ]
+then
+    nodes_start=$1
+    nodes_end=$2
+elif [ $# -eq 1 ]
+then
+    nodes_start=0
+    nodes_end=$1
+else
+    echo "run_suite_r.sh [<start_node>] <end_node>" 1>&2
+    exit 1
+fi
+
 result_folder="results"
 mkdir -p $result_folder
-rm $result_folder/*
 
-./killall.sh
-
-nodes=$1
-
-for i in $(seq 1 $nodes)
+for (( i=$nodes_start; i<$nodes_end; i++ ))
 do
 python run_bench.py --result-folder $result_folder --host node$i compute >/dev/null 2>&1 &
+echo "launched compute node node$i"
 done
 
 popd > /dev/null
